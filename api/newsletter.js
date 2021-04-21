d4a1f9543ecdfbc574a0847e45e2328c23f90c1e
Nco9r <Nicolas929@me.com>
2021-04-21 15:16:54 +0200

    cors

--- api/newsletter.js
+++ api/newsletter.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import Mailchimp from 'mailchimp-api-v3'
-const cors = require('cors');
-app.use(cors());
+
 const API_KEY = process.env.MAILCHIMP_API_KEY
 const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID
 const mailchimp = new Mailchimp(API_KEY)
 
 const app = express()
 app.use(express.json())
+
+const cors = require('cors');
+app.use(cors());
 
 app.post('/subscribe', async(req, res) => {
   const {email: email_address} = req.body
