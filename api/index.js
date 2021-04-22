57f3096f369c596c4722c0abe8f3e69f7751bd35
Nco9r <Nicolas929@me.com>
2021-04-22 14:35:45 +0200

    api

--- api/index.js
+++ api/index.js
@@ -74,6 +74,51 @@ mail();
 
 })
 
+app.post('/devis-mt', (req, res) => {
+  console.log(req.body);
+
+  const sparkPostTransporter = nodemailer.createTransport(
+  sparkPostTransport({
+    sparkPostApiKey: process.env.sparkPostApiKey,
+    endpoint: "https://api.sparkpost.com"
+  })
+);
+
+  async function main() {
+  try {
+    const data = await sparkPostTransporter.sendMail({
+      from: 'noreply@rouxnicolas.fr',
+      to: req.body.email,
+      subject: 'Demande de devis',
+      html: `<p style="font-size : 14px">Bonjour <strong>${req.body.name}</strong>,</p> <p style="font-size : 14px">Nous avons bien reçu votre demande, nous reviendrons vers vous dans les plus brefs délai. </p><p style="font-size : 14px">Merci de votre confiance, à bientot, </p><p style="font-size : 14px"> Nicolas ROUX <br> 06 83 97 65 90 <br> Développeur Web</p>`,
+    });
+    res.setHeader('Content-type', 'application/json')
+    res.send(data)
+  } catch (e) {
+  }
+}
+
+main();
+
+async function mail() {
+  try {
+    const data = await sparkPostTransporter.sendMail({
+      from: 'devis@rouxnicolas.fr',
+      to: 'hello@rouxnicolas.fr',
+      subject: 'Demande de devis',
+      text: `Bonjour vous avez une nouvelle demande de devis de la part de ${req.body.name}. Vous pouvez le joindre au ${req.body.phone} ou part mail ${req.body.email}. Il a choisi la prestation ${req.body.prestation} à la date du ${req.body.date}, sur le lieu ${req.body.lieu}, avec ${req.body.convives} convives.`,
+    });
+    res.setHeader('Content-type', 'application/json')
+    res.send(data)
+  } catch (e){
+
+  }
+}
+
+mail();
+
+})
+
 
 
 
